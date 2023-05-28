42f07c48a398e44d66cdc5246d8e9f34902a1230
hirohata <hirohatatro@gmail.com>
2023-05-28 12:48:44 +0900

    fix udp client

--- src/udp_client.rs
+++ src/udp_client.rs
@@ -2,11 +2,11 @@ use std::net::UdpSocket;
 use std::{io, str};
 
 pub fn communicate(address: &str) -> Result<(), failure::Error> {
-    let socket = UdpSocket::bind(address)?;
+    let socket = UdpSocket::bind("127.0.0.1:0")?;
     loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
-        socket.send_to(input.as_bytes(), address)?;
+        socket.send_to(input.as_bytes(), address).expect("Failed to send data");
 
         let mut buffer = [0u8; 1024];
         socket.recv_from(&mut buffer).expect("failed to receive");
